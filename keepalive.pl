614f165e748e7d8059ab57a0576fa4f27f51f215
jomat <jomat@users.noreply.github.com>
2016-11-12 13:20:52 +0000

    keepalive.pl: use send_raw to send raw commands

--- keepalive.pl
+++ keepalive.pl
@@ -43,7 +43,7 @@ $VERSION = '20140609';
 sub keepalive_message_self {
 	foreach my $server (Irssi::servers()) {
 		my $nick = $server->{nick};
-		$server->command("PING a");
+		$server->send_raw("PING a");
 	}
 }
 
